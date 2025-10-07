09d0698c3b1e56c242511ba97cd7672505083459
Ramachandran-devv <129505485+ramachnadran@users.noreply.github.com>
2025-10-07 15:22:31 +0530

    Update twosums.js

--- twosums.js
+++ twosums.js
@@ -1,1 +1,11 @@
-
+var twoSum = function(nums, target) {
+  const map = new Map();
+  for (let i = 0 ; i < nums.length; i++){
+    const complement = target - nums[i];
+    if(map.has(complement)){
+      return[map.get(complemnt), i ];
+    }
+    map.set(nums[i], i);
+  }
+  return [];
+}
